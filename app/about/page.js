92f268238d3d1141c732b89b69fc6e058ad4543e
eviva2000 <arjomandielaheh@gamin.com>
2024-08-28 16:10:51 +0200

    get number of cabins and add revalidation

--- app/about/page.js
+++ app/about/page.js
@@ -1,7 +1,12 @@
 import Image from "next/image";
 import image1 from "@/public/about-1.jpg";
+import { getCabins } from "../_lib/data-service";
+export const revalidate = 86400; // revalidate data once a day
 
-export default function Page() {
+export default async function Page() {
+  const numCabins = (await getCabins()).length;
+  console.log(numCabins);
+
   return (
     <div className="grid grid-cols-5 gap-x-24 gap-y-32 text-lg items-center">
       <div className="col-span-3">
@@ -18,10 +23,10 @@ export default function Page() {
             and enjoying simple pleasures with family.
           </p>
           <p>
-            Our 8 luxury cabins provide a cozy base, but the real freedom and
-            peace you&rsquo;ll find in the surrounding mountains. Wander through
-            lush forests, breathe in the fresh air, and watch the stars twinkle
-            above from the warmth of a campfire or your hot tub.
+            Our {numCabins} luxury cabins provide a cozy base, but the real
+            freedom and peace you&rsquo;ll find in the surrounding mountains.
+            Wander through lush forests, breathe in the fresh air, and watch the
+            stars twinkle above from the warmth of a campfire or your hot tub.
           </p>
           <p>
             This is where memorable moments are made, surrounded by
