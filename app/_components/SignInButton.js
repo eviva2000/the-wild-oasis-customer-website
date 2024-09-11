74e10f53659d58cd2d0f0fff87a5da78afb13788
eviva2000 <arjomandielaheh@gamin.com>
2024-09-11 21:37:03 +0200

    add signin with server action

--- app/_components/SignInButton.js
+++ app/_components/SignInButton.js
@@ -1,14 +1,19 @@
+import { signInAction } from "../_lib/actions";
+
+// since the entire work flow of the sign in happens in the server , this button component is a server component and we can not use onClick event on this button, instead we can use server action to handle the sign in process
 function SignInButton() {
   return (
-    <button className="flex items-center gap-6 text-lg border border-primary-300 px-10 py-4 font-medium">
-      <img
-        src="https://authjs.dev/img/providers/google.svg"
-        alt="Google logo"
-        height="24"
-        width="24"
-      />
-      <span>Continue with Google</span>
-    </button>
+    <form action={signInAction}>
+      <button className="flex items-center gap-6 text-lg border border-primary-300 px-10 py-4 font-medium">
+        <img
+          src="https://authjs.dev/img/providers/google.svg"
+          alt="Google logo"
+          height="24"
+          width="24"
+        />
+        <span>Continue with Google</span>
+      </button>
+    </form>
   );
 }
 
