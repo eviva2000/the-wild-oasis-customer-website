520d43af88e84628cc94a15cbf4b3cf811cb7cdf
eviva2000 <arjomandielaheh@gamin.com>
2024-09-21 12:43:57 +0200

    revalidating data after updating the profile

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,16 +1,19 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import { supabase } from "./supabase";
 
 export async function updateProfile(formData) {
   const session = await auth();
-  console.log("session from actions @@@@@", session);
+
   // In server actions we dont use try catch block
   if (!session) throw new Error("Not authenticated, please login");
+
   const nationalID = formData.get("nationalID");
   const [nationality, countryFlag] = formData.get("nationality").split("%");
   const nationalIdPattern = /^[a-zA-Z0-9]{6,12}$/;
+
   if (!nationalIdPattern.test(nationalID))
     throw new Error("Invalid national ID");
   const updateData = { nationalID, nationality, countryFlag };
@@ -20,6 +23,8 @@ export async function updateProfile(formData) {
     .eq("id", session.user.guestId);
 
   if (error) throw new Error("Guest could not be updated");
+  //built-in function in Next.js to revalidate the data
+  revalidatePath("/account/profile"); // if we use /account all sub-pages of /account will be revalidated but we don't need it here. We only need /account/profile to be revalidated
 }
 
 export async function signInAction() {
