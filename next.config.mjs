ebe956ddccab052391389523b76778fe810d057b
eviva2000 <arjomandielaheh@gmail.com>
2025-08-19 14:44:13 +0200

    update secrets

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "trtqlrsxbkklxlnkjvtx.supabase.co",
+        hostname: "eyqyeuaguwojbsavaovr.supabase.co",
         port: "",
         pathname: "/storage/v1/object/public/cabin-images/**",
       },
